6ce77bdcb8b99558ba5f207264790d89aa28b2e6
fajarprayoga <fajarprayoga23@gmail.com>
2024-03-07 13:24:13 +0800

    07-03-2024

--- src/page/home/index.js
+++ src/page/home/index.js
@@ -520,7 +520,7 @@ const Home = ({navigation}) => {
               marginRight: windowWidht * 0.02,
               backgroundColor: '#FFFFFF',
             }}>
-            V-24.02.19
+            V-24.03.07
           </Text>
         </SafeAreaView>
         <Footer focus="Home" navigation={navigation} />
